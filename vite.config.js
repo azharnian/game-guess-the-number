7085d56e4623334195519baf381095a98966e2d6
azharnian <anas.sty@gmail.com>
2023-09-19 22:49:00 +0700

    fixed : some issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/game-guess-the-number/",
   plugins: [react()],
   server: {
     host: true
